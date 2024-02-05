ab9fb2c071c6e6c5ac31b61ae214bda1a7c63f74
howard <howard.yang@kiwigame.com>
2024-02-04 21:24:18 -0600

    add idt

--- source/comm/cpu_instr.h
+++ source/comm/cpu_instr.h
@@ -43,6 +43,20 @@ static inline void lgdt(uint32_t start, uint32_t size) {
     __asm__ __volatile__("lgdt %[g]"::[g]"m"(gdt)); // m means a memory address
 }
 
+static inline void lidt(uint32_t start, uint32_t size) {
+    struct {
+        uint16_t limit;
+        uint16_t start15_0;
+        uint16_t start31_16;
+    }idt;
+
+    idt.start31_16 = start >> 16;
+    idt.start15_0 = start & 0xFFFF;
+    idt.limit = size - 1;
+
+    __asm__ __volatile__("lidt %[i]"::[i]"m"(idt)); // m means a memory address
+}
+
 static inline uint32_t read_cr0(void) {
     uint32_t cr0;
     __asm__ __volatile__ ("mov %%cr0, %[v]":[v]"=r"(cr0));
--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -1,6 +1,12 @@
 #include "init.h"
 #include "comm/boot_info.h"
+#include "cpu/cpu.h"
 
 void kernel_init(boot_info_t *boot_info) {
-    
+    cpu_init();
+}
+
+void init_main() {
+    int a = 3 / 0;
+    for (;;) {}
 }
