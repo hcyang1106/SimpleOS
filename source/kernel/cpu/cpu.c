81a722c2a60cb7228bd8fc6e72fd8dbb60fb2633
howard <howard.yang@kiwigame.com>
2024-06-26 01:07:06 -0500

    add idt function

--- source/kernel/cpu/cpu.c
+++ source/kernel/cpu/cpu.c
@@ -1,24 +1,139 @@
 #include "cpu/cpu.h"
 #include "os_cfg.h"
 #include "comm/cpu_instr.h"
+#include "tools/log.h"
+
+void exception_handler_unknown (void);
+void exception_handler_divider (void);
+void exception_handler_Debug (void);
+void exception_handler_NMI (void);
+void exception_handler_breakpoint (void);
+void exception_handler_overflow (void);
+void exception_handler_bound_range (void);
+void exception_handler_invalid_opcode (void);
+void exception_handler_device_unavailable (void);
+void exception_handler_double_fault (void);
+void exception_handler_invalid_tss (void);
+void exception_handler_segment_not_present (void);
+void exception_handler_stack_segment_fault (void);
+void exception_handler_general_protection (void);
+void exception_handler_page_fault (void);
+void exception_handler_fpu_error (void);
+void exception_handler_alignment_check (void);
+void exception_handler_machine_check (void);
+void exception_handler_smd_exception (void);
+void exception_handler_virtual_exception (void);
 
 static segment_desc_t gdt_table[GDT_TABLE_SIZE];
 static gate_desc_t idt_table[IDT_TABLE_SIZE];
 
-void exception_handler_unknown(void);
-void exception_handler_divider(void);
-
+// print regs when exception/interrupt happens
+static void dump_core_regs (exception_frame_t * frame) {
+    log_printf("IRQ: %d, error code: %d.", frame->num, frame->error_code);
+    log_printf("CS: %d\nDS: %d\nES: %d\nSS: %d\nFS: %d\nGS: %d",
+               frame->cs, frame->ds, frame->es, frame->ds, frame->fs, frame->gs
+    );
+    log_printf("EAX: 0x%x\n"
+                "EBX: 0x%x\n"
+                "ECX: 0x%x\n"
+                "EDX: 0x%x\n"
+                "EDI: 0x%x\n"
+                "ESI: 0x%x\n"
+                "EBP: 0x%x\n"
+                "ESP: 0x%x\n",
+               frame->eax, frame->ebx, frame->ecx, frame->edx,
+               frame->edi, frame->esi, frame->ebp, frame->esp);
+    log_printf("EIP: 0x%x\nEFLAGS: 0x%x\n", frame->eip, frame->eflags);
+}
 
 static void do_default_handler(exception_frame_t *frame, char *msg) {
-    for (;;) {}
+    log_printf("--------------------");
+    log_printf("IRQ/Exception happend: %s", msg);
+    dump_core_regs(frame);
+
+    for (;;) {
+        hlt();
+    }
 }
 
 void do_handler_unknown(exception_frame_t *frame) {
-    do_default_handler(frame, "unknown exception");
+    do_default_handler(frame, "Unknown Exception");
 }
 
 void do_handler_divider(exception_frame_t *frame) {
-    do_default_handler(frame, "Divider exception");
+    do_default_handler(frame, "Divider Exception");
+}
+
+void do_handler_Debug(exception_frame_t * frame) {
+	do_default_handler(frame, "Debug Exception");
+}
+
+void do_handler_NMI(exception_frame_t * frame) {
+	do_default_handler(frame, "NMI Interrupt");
+}
+
+void do_handler_breakpoint(exception_frame_t * frame) {
+	do_default_handler(frame, "Breakpoint");
+}
+
+void do_handler_overflow(exception_frame_t * frame) {
+	do_default_handler(frame, "Overflow Exception");
+}
+
+void do_handler_bound_range(exception_frame_t * frame) {
+	do_default_handler(frame, "BOUND Range Exceeded Exception");
+}
+
+void do_handler_invalid_opcode(exception_frame_t * frame) {
+	do_default_handler(frame, "Invalid Opcode Exception");
+}
+
+void do_handler_device_unavailable(exception_frame_t * frame) {
+	do_default_handler(frame, "Device Not Available");
+}
+
+void do_handler_double_fault(exception_frame_t * frame) {
+	do_default_handler(frame, "Double Fault");
+}
+
+void do_handler_invalid_tss(exception_frame_t * frame) {
+	do_default_handler(frame, "Invalid TSS Exception");
+}
+
+void do_handler_segment_not_present(exception_frame_t * frame) {
+	do_default_handler(frame, "Segment Not Present Exception");
+}
+
+void do_handler_stack_segment_fault(exception_frame_t * frame) {
+	do_default_handler(frame, "Stack Segment Fault");
+}
+
+void do_handler_general_protection(exception_frame_t * frame) {
+	do_default_handler(frame, "General Protection");
+}
+
+void do_handler_page_fault(exception_frame_t * frame) {
+	do_default_handler(frame, "Page Fault Exception");
+}
+
+void do_handler_fpu_error(exception_frame_t * frame) {
+	do_default_handler(frame, "X87 FPU Floating Point Error");
+}
+
+void do_handler_alignment_check(exception_frame_t * frame) {
+	do_default_handler(frame, "Alignment Check");
+}
+
+void do_handler_machine_check(exception_frame_t * frame) {
+	do_default_handler(frame, "Machine Check");
+}
+
+void do_handler_smd_exception(exception_frame_t * frame) {
+	do_default_handler(frame, "SIMD Floating Point Exception");
+}
+
+void do_handler_virtual_exception(exception_frame_t * frame) {
+	do_default_handler(frame, "Virtualization Exception");
 }
 
 void segment_desc_set(int selector, uint32_t base, uint32_t limit, uint16_t attr) {
@@ -38,14 +153,15 @@ void segment_desc_set(int selector, uint32_t base, uint32_t limit, uint16_t attr
     desc->base31_24 = (base >> 24) & 0xFF;
 }
 
-void gate_desc_set(gate_desc_t *desc, uint16_t selector, uint32_t offset, uint16_t attr) {
+// exported
+static void gate_desc_set(gate_desc_t *desc, uint16_t selector, uint32_t offset, uint16_t attr) {
     desc->offset15_0 = offset &0xFFFF;
     desc->selector = selector;
     desc->attr = attr;
     desc->offset31_16 = offset >> 16; 
 }
 
-void init_gdt(void) {
+static void init_gdt(void) {
     for (int i = 0; i < GDT_TABLE_SIZE; i++) {
         segment_desc_set(i * sizeof(segment_desc_t), 0, 0, 0);
     }
@@ -58,19 +174,137 @@ void init_gdt(void) {
     lgdt((uint32_t)gdt_table, sizeof(gdt_table));
 }
 
-void init_idt(void) {
+// initialize 8259 chip (a prgrammable interrupt controller)
+static void init_pic(void) {
+    outb(PIC0_ICW1, PIC_ICW1_ALWAYS_1 | PIC_ICW1_ICW4);
+    // interrupt number starts from 0x20 (number smaller than 0x20 are already occupied by preset interrupts)
+    outb(PIC0_ICW2, IRQ_PIC_START);
+    // this means IRQ2 is connected with another 8259 chip
+    outb(PIC0_ICW3, 1 << 2);
+    // 8086 mode
+    outb(PIC0_ICW4, PIC_ICW4_8086);
+
+    outb(PIC1_ICW1, PIC_ICW1_ICW4 | PIC_ICW1_ALWAYS_1);
+    // the interrupt number starts from 0x20 + 8
+    outb(PIC1_ICW2, IRQ_PIC_START + 8);
+    // connected to PIC0
+    outb(PIC1_ICW3, 2);
+    outb(PIC1_ICW4, PIC_ICW4_8086);
+
+    // disable all interrupts except the signal sent to IRQ2
+    // since we're masking PIC1, there's no need to disable IRQ2 of PCI0;
+    outb(PIC0_IMR, 0xFF & ~(1 << 2));
+    outb(PIC1_IMR, 0xFF);
+}
+
+// exception handler must end with "iret", therefore we must write the function in assembly.
+// iret is used to pop back regs
+static void init_idt(void) {
     for (int i = 0; i < IDT_TABLE_SIZE; i++) {
-        gate_desc_set(idt_table + i, KERNEL_SELECTOR_CS, (uint32_t)exception_handler_unknown, GATE_P_PRESENT | GATE_DPL0 |
-         GATE_D | GATE_TYPE_INT); // offset is function address
+        irq_install(i, exception_handler_unknown);
+        // gate_desc_set(idt_table + i, KERNEL_SELECTOR_CS, (uint32_t)exception_handler_unknown, GATE_P_PRESENT | GATE_DPL0 |
+        //  GATE_D | GATE_TYPE_INT); // offset is function address
     }
 
-    gate_desc_set(idt_table + IRQ0_DE, KERNEL_SELECTOR_CS, (uint32_t)exception_handler_divider, GATE_P_PRESENT | GATE_DPL0 |
-         GATE_D | GATE_TYPE_INT);
+    irq_install(IRQ0_DE, exception_handler_divider);
+    irq_install(IRQ1_DB, exception_handler_Debug);
+    irq_install(IRQ2_NMI, exception_handler_NMI);
+    irq_install(IRQ3_BP, exception_handler_breakpoint);
+    irq_install(IRQ4_OF, exception_handler_overflow);
+    irq_install(IRQ5_BR, exception_handler_bound_range);
+    irq_install(IRQ6_UD, exception_handler_invalid_opcode);
+    irq_install(IRQ7_NM, exception_handler_device_unavailable);
+    irq_install(IRQ8_DF, exception_handler_double_fault);
+    irq_install(IRQ10_TS, exception_handler_invalid_tss);
+    irq_install(IRQ11_NP, exception_handler_segment_not_present);
+    irq_install(IRQ12_SS, exception_handler_stack_segment_fault);
+    irq_install(IRQ13_GP, exception_handler_general_protection);
+    irq_install(IRQ14_PF, exception_handler_page_fault);
+    irq_install(IRQ16_MF, exception_handler_fpu_error);
+    irq_install(IRQ17_AC, exception_handler_alignment_check);
+    irq_install(IRQ18_MC, exception_handler_machine_check);
+    irq_install(IRQ19_XM, exception_handler_smd_exception);
+    irq_install(IRQ20_VE, exception_handler_virtual_exception);
 
     lidt((uint32_t)idt_table, sizeof(idt_table));
+
+    init_pic();
+}
+
+// exported
+void irq_install(int irq_num, irq_handler_t handler) {
+    gate_desc_set(idt_table + irq_num, KERNEL_SELECTOR_CS, (uint32_t)handler, GATE_P_PRESENT | GATE_DPL0 |
+         GATE_D | GATE_TYPE_INT);
+}
+
+void pic_send_eoi(int irq_num) {
+    irq_num -= IRQ_PIC_START;
+
+    if (irq_num >= 8) {
+        outb(PIC1_OCW2, PIC_OCW2_EOI);
+    }
+
+    outb(PIC0_OCW2, PIC_OCW2_EOI);
+}
+
+void irq_enable(int irq_num) {
+    if (irq_num < IRQ_PIC_START) {
+        return;
+    }
+
+    int irq_bit = irq_num - IRQ_PIC_START;
+    if (irq_bit < 8) {
+        uint8_t mask = inb(PIC0_IMR) & ~(1 << irq_bit);
+        outb(PIC0_IMR, mask);
+        return;
+    }
+    irq_bit -= 8;
+    uint8_t mask = inb(PIC1_IMR) & ~(1 << irq_bit);
+    outb(PIC1_IMR, mask);
+    return;
+}
+
+void irq_disable(int irq_num) {
+    if (irq_num < IRQ_PIC_START) {
+        return;
+    }
+
+    int irq_bit = irq_num - IRQ_PIC_START;
+    if (irq_bit < 8) {
+        uint8_t mask = inb(PIC0_IMR) | (1 << irq_bit);
+        outb(PIC0_IMR, mask);
+        return;
+    }
+    irq_bit -= 8;
+    uint8_t mask = inb(PIC1_IMR) | (1 << irq_bit);
+    outb(PIC1_IMR, mask);
+    return;
+}
+
+void irq_enable_global(void) {
+    sti(); // it sets EFLAGS IF flag to 1
+}
+
+void irq_disable_global(void) {
+    cli(); // it sets EFLAGS IF flag to 0
 }
 
 void cpu_init(void) {
     init_gdt();
     init_idt();
+}
+
+int gdt_alloc_desc(void) {
+    // i should begin with 1 because 0 is not used
+    for (int i = 1; i < GDT_TABLE_SIZE; i++) {
+        if (gdt_table[i].attr == 0) {
+            return i * sizeof(segment_desc_t);
+        }
+    }
+
+    return -1;
+}
+
+void switch_to_tss(int tss_sel) {
+    far_jump(tss_sel, 0);
 }
--- source/kernel/include/cpu/cpu.h
+++ source/kernel/include/cpu/cpu.h
@@ -3,9 +3,23 @@
 
 #include "comm/types.h"
 
+#define EFLAGS_IF           (1 << 9)
+#define EFLAGS_DEFAULT      (1 << 1)
+
+typedef struct _tss_t {
+    uint32_t prelink;
+    uint32_t esp0, ss0, esp1, ss1, esp2, ss2;
+    uint32_t cr3;
+    uint32_t eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    uint32_t es, cs, ss, ds, fs, gs;
+    uint32_t ldt;
+    uint32_t iomap;
+}tss_t;
+
 #pragma pack(1)
 
-typedef struct _segment_desc_t {
+// _segment_desc_t can be ignored
+typedef struct _segment_desc_t { 
     uint16_t limit15_0;
     uint16_t base15_0;
     uint8_t base23_16;
@@ -29,9 +43,22 @@ typedef struct _exception_frame_t {
     uint32_t eip, cs, eflags;
 }exception_frame_t;
 
+typedef void(*irq_handler_t)(void); // defines a type irq_handler_t which is a pointer to void(*)(void)
+
 void cpu_init(void);
+void pic_send_eoi(int irq_num);
+
+void irq_install(int irq_num, irq_handler_t handler);
 void segment_desc_set(int selector, uint32_t base, uint32_t limit, uint16_t attr);
-void gate_desc_set(gate_desc_t *desc, uint16_t selector, uint32_t offset, uint16_t attr);
+
+void irq_enable_global(void);
+void irq_disable_global(void);
+void irq_enable(int irq_num);
+void irq_disable(int irq_num);
+
+int gdt_alloc_desc(void); // find an unused space in gdt
+
+void switch_to_tss(int tss_sel);
 
 // for gdt descriptors
 #define SEG_G (1 << 15) // granularity is 4KB
@@ -46,16 +73,57 @@ void gate_desc_set(gate_desc_t *desc, uint16_t selector, uint32_t offset, uint16
 
 #define SEG_TYPE_CODE (1 << 3)
 #define SEG_TYPE_DATA (0 << 3)
+#define SEG_TSS (9 << 0)
 
-#define SEG_TYPE_RW (1 << 1)
+#define SEG_TYPE_RW (1 << 1) // both read and write available
 
 // for idt descriptors
 #define GATE_P_PRESENT (1 << 15)
 #define GATE_DPL0 (0 << 13)
 #define GATE_DPL3 (3 << 13)
 #define GATE_D (1 << 11)
-#define GATE_TYPE_INT (6 << 8)
+#define GATE_TYPE_INT (0xE << 8)
 
 #define IRQ0_DE 0
+#define IRQ1_DB 1
+#define IRQ2_NMI 2
+#define IRQ3_BP 3
+#define IRQ4_OF 4
+#define IRQ5_BR 5
+#define IRQ6_UD 6
+#define IRQ7_NM 7
+#define IRQ8_DF 8
+#define IRQ10_TS 10
+#define IRQ11_NP 11
+#define IRQ12_SS 12
+#define IRQ13_GP 13
+#define IRQ14_PF 14
+#define IRQ16_MF 16
+#define IRQ17_AC 17
+#define IRQ18_MC 18
+#define IRQ19_XM 19
+#define IRQ20_VE 20
+
+#define IRQ0_TIMER          0x20
+
+#define PIC0_ICW1			0x20
+#define PIC0_ICW2			0x21
+#define PIC0_ICW3			0x21
+#define PIC0_ICW4			0x21
+#define PIC0_OCW2			0x20
+#define PIC0_IMR			0x21
+
+#define PIC1_ICW1			0xa0
+#define PIC1_ICW2			0xa1
+#define PIC1_ICW3			0xa1
+#define PIC1_ICW4			0xa1
+#define PIC1_OCW2			0xa0
+#define PIC1_IMR			0xa1
+
+#define PIC_ICW1_ICW4		(1 << 0)
+#define PIC_ICW1_ALWAYS_1	(1 << 4)
+#define PIC_ICW4_8086	    (1 << 0)
+#define PIC_OCW2_EOI		(1 << 5)
+#define IRQ_PIC_START		0x20
 
 #endif
