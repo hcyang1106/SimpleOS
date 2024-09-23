7e091f803b4faa809dedc8d657521260f9e991ce
howard <howard.yang@kiwigame.com>
2024-09-23 16:40:39 -0500

    1. add task related system calls
    add file related functions to task

--- source/kernel/core/task.c
+++ source/kernel/core/task.c
@@ -8,25 +8,32 @@
 #include "cpu/cpu.h"
 #include "core/memory.h"
 #include "cpu/mmu.h"
+#include "core/syscall.h"
+#include "comm/elf.h"
+#include "fs/fs.h"
 
 static task_manager_t task_manager;
 static uint32_t idle_task_stack[1024];
+static task_t task_table[TASK_NUM];
+static mutex_t task_table_mutex;
 
 void main_task_entry(int, int); // to test whether arguments matter
 
 static void idle_task_entry(void) {
+    int i = 0;
     for (;;) {
+        i++;
         hlt();
     }
 }
 
 void simple_switch(uint32_t **from , uint32_t *to);
 
-static void tss_init(task_t *task, int flag, uint32_t entry, uint32_t esp) {
+static int tss_init(task_t *task, int flag, uint32_t entry, uint32_t esp) {
     // a process can be created a long time after the kernel runs
     // so we're unsure where is empty in gdt, so this alloc func is needed
     int tss_sel = gdt_alloc_desc(); 
-    if (tss_sel <= 0) {
+    if (tss_sel < 0) {
         log_printf("alloc for tss in gdt failed");
         goto tss_init_failed;
     }
@@ -69,38 +76,57 @@ static void tss_init(task_t *task, int flag, uint32_t entry, uint32_t esp) {
     
     task->tss.cr3 = page_dir;
     task->tss_sel = tss_sel;
-    return;
+
+    return 0;
 
 tss_init_failed:
     gdt_free_sel(tss_sel);
     if (kernel_stack) {
         mem_free_page((uint32_t)kernel_stack, STACK_ZERO_PAGE_COUNT);
     }
-    return;
+    return -1;
+}
+
+void tss_uninit(tss_t tss, int sel) {
+    gdt_free_sel(sel);
+    mem_free_page(tss.esp0 - MEM_PAGE_SIZE, 1); // why? because tss.esp never changes
+    memory_destroy_uvm(tss.cr3);
 }
 
 // cr3 is not replaced when doing task switching
-void task_init(task_t *task, const char *name, int flag, uint32_t entry, uint32_t esp) {
+int task_init(task_t *task, const char *name, int flag, uint32_t entry, uint32_t esp) {
     // recall the definition of null pointer
     // null pointer is unequal to any pointer pointing to an object or function
     ASSERT(task != (task_t*)0);
-    tss_init(task, flag, entry, esp); // this is for hardware switching
+    int ret = tss_init(task, flag, entry, esp); // this is for hardware switching
+    if (ret < 0) {
+        return -1;
+    }
 
+    task->pid = (uint32_t)task;
     kernel_strncpy(task->name, name, TASK_NAME_SIZE);
+    task->parent = (task_t*)0;
     task->state = TASK_CREATED;
+    task->status = 0;
     task->curr_tick = TASK_TIME_TICKS_DEFAULT;
     task->sleep_tick = 0;
 
+    task->heap_start = 0;
+    task->heap_end = 0;
+
     list_node_init(&task->all_node);
     list_node_init(&task->run_node);
-    list_node_init(&task->wait_node);
+    // list_node_init(&task->wait_node);
+
+    kernel_memset(&task->file_table, 0, sizeof(task->file_table));
 
     irq_state_t state = irq_enter_protection();
 
     list_insert_last(&task_manager.task_list, &task->all_node);
-    task_set_ready(task);
     
     irq_leave_protection(state);
+
+    return 0;
 
     // simple task switching
     // uint32_t *p_esp = (uint32_t*)esp;
@@ -116,12 +142,26 @@ void task_init(task_t *task, const char *name, int flag, uint32_t entry, uint32_
     // }
 }
 
+void task_start(task_t *task) {
+    irq_state_t state = irq_enter_protection();
+    task_set_ready(task);
+    irq_leave_protection(state);
+}
+
+void task_uninit(task_t *task) {
+    tss_uninit(task->tss, task->tss_sel);
+    kernel_memset((void*)task, 0, sizeof(task_t));
+}
+
 void task_switch_from_to(task_t *from, task_t *to) {
     switch_to_tss(to->tss_sel); // this is for hardware switching
     // simple_switch(&from->stack, to->stack);
 }
 
 void task_manager_init(void) {
+    kernel_memset(task_table, 0, sizeof(task_table));
+    mutex_init(&task_table_mutex);
+
     int sel = gdt_alloc_desc();
     segment_desc_set(sel, 0, 0xFFFFFFFF,
         SEG_P_PRESENT | SEG_DPL3 | SEG_S_NORMAL | SEG_TYPE_CODE | SEG_TYPE_RW | SEG_D
@@ -140,6 +180,7 @@ void task_manager_init(void) {
     list_init(&task_manager.sleep_list);
     // stack grows from high to low and esp moves downwards first before pushing, so set as 1024 instead of 1023 
     task_init(&task_manager.idle_task, "idle task", TASK_FLAG_SYSTEM, (uint32_t)idle_task_entry, (uint32_t)&idle_task_stack[1024]);
+    task_start(&task_manager.idle_task);
 }
 
 void main_task_init(void) {
@@ -151,7 +192,10 @@ void main_task_init(void) {
     
     // at first I wrote (uint32_t)&s_main_task + copy_size, this is wrong
     task_init(&task_manager.main_task, "main task", TASK_FLAG_NORMAL,
-             (uint32_t)main_task_entry, (uint32_t)main_task_entry + copy_size);
+             (uint32_t)main_task_entry, (uint32_t)main_task_entry + page_count * MEM_PAGE_SIZE);
+
+    task_manager.main_task.heap_start = (uint32_t)&e_main_task;
+    task_manager.main_task.heap_end = (uint32_t)&e_main_task;
 
     write_tr(task_manager.main_task.tss_sel);
     task_manager.curr_task = &task_manager.main_task;
@@ -164,9 +208,11 @@ void main_task_init(void) {
     uint32_t page_dir = task_manager.main_task.tss.cr3;
     mmu_set_page_dir(page_dir); 
     
-    // we want to paste it soe definitely with PTE_W
+    // we want to paste it so definitely with PTE_W
     alloc_mem_for_task(page_dir, page_count, (uint32_t)main_task_entry, PTE_P | PTE_W | PTE_U);
     kernel_memcpy(main_task_entry, &s_main_task, copy_size);
+
+    task_start(&task_manager.main_task);
 }
 
 task_t *task_main_task(void) {
@@ -189,7 +235,7 @@ void task_set_unready(task_t *task) {
 }
 
 // give up using cpu but insert into ready list right away
-void sys_sched_yield(void) {
+void sys_yield(void) {
     irq_state_t state = irq_enter_protection();
 
     // only the current task is in ready list
@@ -203,9 +249,87 @@ void sys_sched_yield(void) {
     irq_leave_protection(state);
 }
 
+void sys_exit(int status) {
+    task_t *task = task_current();
+    for (int fd = 0; fd < OPEN_FILE_NUM; fd++) {
+        file_t *file = task->file_table[fd];
+        if (file) {
+            sys_close(fd);
+            task->file_table[fd] = (file_t*)0;
+        }
+    }
+
+    int set_ready_main = 0;
+    mutex_lock(&task_table_mutex);
+    for (int i = 0; i < OPEN_FILE_NUM; i++) {
+        task_t *t = task_table + i;
+        if (t->parent == task) {
+            t->parent = &task_manager.main_task; // all child processes go under main_task
+            if (t->state == TASK_ZOMBIE) {
+                set_ready_main = 1; // main_task has to be woken up
+            }
+        }
+    }
+    mutex_unlock(&task_table_mutex);
+
+    irq_state_t state = irq_enter_protection();
+
+    // we wake it up here if main_task is different from parent
+    // otherwise it they are the same and it will be waken up in the following code
+    if (set_ready_main && task->parent != &task_manager.main_task && 
+        task_manager.main_task.state == TASK_WAITING) {
+        task_set_ready(&task_manager.main_task);
+    }
+
+    if (task->parent->state == TASK_WAITING) {
+        task_set_ready(task->parent);
+    }
+    
+    task->state = TASK_ZOMBIE;
+    task->status = status;
+    task_set_unready(task);
+    task_dispatch();
+    irq_leave_protection(state);
+}
+
+// wait until a child process has exit
+int sys_wait(int *status) {
+    task_t *curr_task = task_current();
+
+    while (1) {
+        mutex_lock(&task_table_mutex);
+        for (int i = 0; i < TASK_NUM; i++) {
+            task_t *task = task_table + i;
+            if (task->parent != curr_task) {
+                continue;
+            }
+
+            if (task->state == TASK_ZOMBIE) {
+                int pid = task->pid;
+                *status = task->status;
+                // resource release
+                memory_destroy_uvm(task->tss.cr3);
+                mem_free_page(task->tss.esp0 - MEM_PAGE_SIZE, 1); // why?
+                kernel_memset(task, 0, sizeof(task_t));
+                mutex_unlock(&task_table_mutex);
+                return pid;
+            }
+        }
+        mutex_unlock(&task_table_mutex);
+
+        irq_state_t state = irq_enter_protection();
+        task_set_unready(curr_task);
+        curr_task->state = TASK_WAITING;
+        task_dispatch();
+        irq_leave_protection(state);
+    }
+    
+    return 0;
+}
+
 // select the next task and do switching
 void task_dispatch(void) {
-    // this function will not only exist in sys_sched_yield
+    // this function will not only exist in sys_yield
     // it may be called independently, so we need protection
     irq_state_t state = irq_enter_protection();
 
@@ -261,7 +385,7 @@ void task_time_tick(void) {
     task_dispatch();
 }
 
-void sys_sleep(uint32_t ms) {
+void sys_msleep(uint32_t ms) {
     irq_state_t state = irq_enter_protection();
 
     int ticks = (ms + (OS_TICK_MS - 1)) / OS_TICK_MS;
@@ -270,6 +394,10 @@ void sys_sleep(uint32_t ms) {
     task_dispatch();
 
     irq_leave_protection(state);
+}
+
+uint32_t sys_getpid(void) {
+    return task_current()->pid;
 }
 
 void task_set_sleep(task_t *task, int ticks) {
@@ -285,6 +413,340 @@ void task_set_sleep(task_t *task, int ticks) {
 // so state is not set in unready/unsleep
 void task_set_unsleep(task_t *task) {
     list_remove_node(&task_manager.sleep_list, &task->run_node);
+}
+
+static task_t *alloc_task(void) {
+    task_t *task = (task_t*)0;
+
+    mutex_lock(&task_table_mutex);
+
+    for (int i = 0; i < TASK_NUM; i++) {
+        if (task_table[i].pid == 0) {
+            task = &task_table[i];
+            break;
+        }
+    }
+
+    mutex_unlock(&task_table_mutex);
+
+    return task;
+}
+
+static void free_task(task_t *task) {
+    mutex_lock(&task_table_mutex);
+
+    task->pid = 0;
+
+    mutex_unlock(&task_table_mutex);
+}
+
+static void copy_opened_files(task_t *parent, task_t *child) {
+    for (int i = 0; i < OPEN_FILE_NUM; i++) {
+        file_t *file = *(parent->file_table + i);
+        if (file) {
+            *(child->file_table + i) = file;
+            file_inc_ref(file);
+        }
+    }
+}
+
+// child process will start executing from the "next instruction of lcall"
+// the return value of below function is for parent process
+// child process will not go through this,
+// it only starts from the instruction from "next instruction of lcall"
+// so to return zero for child process, we modify eax
+// child process starts from privilege level 3
+
+// high level idea of fork: copy every registers and "create a new page table",
+// making the same virt addr point to a copied phy mem
+int sys_fork(void) {
+    task_t *parent = task_current();
+    task_t *child = alloc_task();
+    if (!child) {
+        goto fork_failed;
+    }
+
+    syscall_frame_t *frame = (syscall_frame_t*)(parent->tss.esp0 - sizeof(syscall_frame_t)); // why?
+
+    int status = task_init(child, parent->name, 0, frame->eip,
+                           frame->esp + sizeof(uint32_t)*SYSCALL_PARAM_COUNT); // clean up params pushed
+    if (status < 0) {
+        goto fork_failed;
+    }
+
+    copy_opened_files(parent, child);
+
+    child->tss.cs = frame->cs;
+    child->tss.ds = frame->ds;
+    child->tss.es = frame->es;
+    child->tss.fs = frame->fs;
+    child->tss.gs = frame->gs;
+    child->tss.eflags = frame->eflags;
+
+    child->tss.ebx = frame->ebx;
+    // child->tss.eax = frame->eax;
+    child->tss.eax = 0;
+    child->tss.ecx = frame->ecx;
+    child->tss.edx = frame->edx;
+    child->tss.esi = frame->esi;
+    child->tss.edi = frame->edi;
+    child->tss.ebp = frame->ebp;
+
+    child->parent = parent;
+    // should not use the same page table, 
+    // otherwise two processes will modify the same stack
+    // child->tss.cr3 = parent->tss.cr3;
+    uint32_t cr3 = memory_copy_uvm(parent->tss.cr3);
+    if (cr3 < 0) {
+        goto fork_failed;
+    } else {
+        child->tss.cr3 = cr3;
+    }
+
+    task_start(child);
+    
+    return child->pid;
+
+fork_failed:
+    if (child) {
+        free_task(child);
+    }
+    if (status) {
+        task_uninit(child);
+    }
+
+    return -1;
+}
+
+static int load_phdr(int file, Elf32_Phdr* phdr, uint32_t page_dir) {
+    int page_count = up(phdr->p_memsz, MEM_PAGE_SIZE) / MEM_PAGE_SIZE;
+    int ret = alloc_mem_for_task(page_dir, page_count, phdr->p_vaddr, PTE_P | PTE_U | PTE_W);
+    if (ret < 0) {
+        log_printf("alloc mem for task failed");
+        return -1;
+    }
+
+    // move the pos in file to the start of doing copy
+    if (sys_lseek(file, phdr->p_offset, 0) < 0) {
+        log_printf("lseek failed");
+        return -1;
+    }
+
+    // target address, however it can only be used with new page dir
+    // and we're currently using old page pir
+    // vaddr 4096 aligned, if not then we cannot do the stuffs in while loop
+    uint32_t vaddr = phdr->p_vaddr; 
+    uint32_t size = phdr->p_filesz;
+
+    // the physical pages may not be continuous
+    while (size > 0) {
+        int curr_size = (size > MEM_PAGE_SIZE) ? MEM_PAGE_SIZE : size;
+        uint32_t paddr = memory_get_paddr(page_dir, vaddr);
+        int ret = sys_read(file, (char*)paddr, curr_size);
+        if (ret < 0) {
+            log_printf("sys read failed");
+            return -1;
+        }
+        
+        size -= curr_size;
+        vaddr += curr_size;
+    }
+
+    return 0;
+}
+
+static uint32_t load_elf_file(task_t *task, const char *name, uint32_t page_dir) {
+    Elf32_Ehdr elf_hdr;
+    Elf32_Phdr elf_phdr;
+
+    // load from disk to mem (it is placed in kernel bss part)
+    int file = sys_open(name, 0);
+    if (file < 0) {
+        log_printf("open %s failed", name);
+        goto load_elf_failed;
+    }
+
+    // validate elf hdr
+    int cnt = sys_read(file, (char*)&elf_hdr, sizeof(Elf32_Ehdr));
+    if (cnt < sizeof(Elf32_Ehdr)){
+        log_printf("read elf hdr error. size=%d", cnt);
+        goto load_elf_failed;
+    }
+
+    // validate header
+    if ((elf_hdr.e_ident[0] != 0x7f) || (elf_hdr.e_ident[1] != 'E')
+        || (elf_hdr.e_ident[2] != 'L') || (elf_hdr.e_ident[3] != 'F')) {
+        log_printf("elf ident validation failed");
+        goto load_elf_failed; 
+    }
+
+    
+    // elf file consists of program header 0, program header 1...
+    // e_phoff is the starting position of program header 0 
+    uint32_t e_phoff = elf_hdr.e_phoff;
+    for (int i = 0; i < elf_hdr.e_phnum; i++, e_phoff += elf_hdr.e_phentsize) {
+        if (sys_lseek(file, e_phoff, 0) < 0) {
+            log_printf("sys_lseek failed");
+            goto load_elf_failed;
+        }
+
+        cnt = sys_read(file, (char*)&elf_phdr, sizeof(elf_phdr));
+        if (cnt < sizeof(elf_phdr)) {
+            log_printf("sys_read failed");
+            goto load_elf_failed;
+        }
+
+        if (elf_phdr.p_type != PT_LOAD || elf_phdr.p_vaddr < MEM_TASK_BASE) {
+            continue;
+        }
+
+        int ret = load_phdr(file, &elf_phdr, page_dir);
+        if (ret < 0) {
+            log_printf("load program failed");
+            goto load_elf_failed;
+        }
+
+        task->heap_start = elf_phdr.p_vaddr + elf_phdr.p_memsz;
+        task->heap_end = task->heap_start;
+    }
+
+    sys_close(file);
+
+    return elf_hdr.e_entry;
+
+load_elf_failed:
+    if (file) {
+        sys_close(file);
+    }
+    return -1;
+}
+
+static int copy_args(char *to, uint32_t page_dir, int argc, char **argv) {
+    task_args_t task_args;
+    task_args.argc = argc;
+    task_args.argv = (char**)(to + sizeof(task_args_t));
+    // ret address is not needed because not returning
+    
+    // copy task_args part
+    int ret = memory_copy_uvm_data((uint32_t)to, page_dir, (uint32_t)&task_args, sizeof(task_args_t));
+    if (ret < 0) {
+        return -1;
+    }
+
+    // args part
+    char *arg_dest = to + sizeof(task_args_t) + sizeof(char*) * (argc + 1);
+    // to is virt addr of new page dir
+    // char **p_arg_dest = (char**)(to + sizeof(task_args_t));
+    char **p_arg_dest = (char**)memory_get_paddr(page_dir, (uint32_t)(to + sizeof(task_args_t)));
+    if (!p_arg_dest) {
+        return -1;
+    }
+
+    for (int i = 0; i < argc; i++) {
+        int len = kernel_strlen(argv[i]) + 1; // remember the null char
+        int ret = memory_copy_uvm_data((uint32_t)arg_dest, page_dir, (uint32_t)argv[i], len);
+        if (ret < 0) {
+            return -1;
+        }
+        p_arg_dest[i] = arg_dest;
+        arg_dest += len;
+    }
+
+    p_arg_dest[argc] = (char*)0;
+
+    return 0;
+}
+
+// after exec the code and data will be completely replaced
+// child of fork does not go through the sys_fork code
+// execve does (it is changing itself)
+int sys_execve(char *name, char **argv, char **env) {
+    task_t *task = task_current();
+
+    kernel_strncpy(task->name, get_file_name(name), TASK_NAME_SIZE);
+
+    uint32_t old_page_dir = task->tss.cr3;
+
+    uint32_t new_page_dir = memory_create_uvm();
+    if (!new_page_dir) {
+        goto exec_failed;
+    }
+
+    // this also sets up page tables
+    uint32_t entry = load_elf_file(task, name, new_page_dir);
+    if (!entry) {
+        goto exec_failed;
+    }
+
+    uint32_t stack_top = MEM_TASK_STACK_TOP - MEM_TASK_ARG_SIZE;
+    int err = alloc_mem_for_task(
+        new_page_dir, MEM_TASK_STACK_SIZE / MEM_PAGE_SIZE,
+        MEM_TASK_STACK_TOP - MEM_TASK_STACK_SIZE, PTE_P | PTE_U | PTE_W
+    );
+    if (err < 0) {
+        goto exec_failed;
+    }
+
+    int argc = strings_count(argv);
+    err = copy_args((uint8_t*)stack_top, new_page_dir, argc, argv);
+    if (err < 0) {
+        goto exec_failed;
+    }
+
+    syscall_frame_t *frame = (syscall_frame_t*)(task->tss.esp0 - sizeof(syscall_frame_t));
+    frame->eip = entry;
+    frame->eax = frame->ebx = frame->ecx = frame->edx = 0;
+    frame->esi = frame->edi = frame->ebp = 0;
+    frame->eflags = EFLAGS_IF | EFLAGS_DEFAULT;
+    frame->esp = stack_top - sizeof(uint32_t) * SYSCALL_PARAM_COUNT;
+    // cs ss are the same so not set
+
+    task->tss.cr3 = new_page_dir;
+    // should set cr3 to change page dir immediately
+    mmu_set_page_dir(new_page_dir);
+    // memory_destroy_uvm(old_page_dir); TODO: adding this line leads to error
+
+    return 0;
+
+exec_failed:
+    if (new_page_dir) {
+        memory_destroy_uvm(new_page_dir);
+    }
+
+    task->tss.cr3 = old_page_dir;
+    mmu_set_page_dir(old_page_dir);
+
+    return -1;
+}
+
+file_t *task_file(int fd) {
+    file_t *file = (file_t*)0;
+    if (fd >= 0 && fd < OPEN_FILE_NUM) {
+        file = task_current()->file_table[fd];
+    }
+
+    return file; 
+}
+
+int task_alloc_fd(file_t *file) {
+    task_t *task = task_current();
+    for (int i = 0; i < OPEN_FILE_NUM; i++) {
+        if (task->file_table[i]) {
+            continue;
+        }
+        task->file_table[i] = file;
+        return i;
+    }
+    
+    return -1;
+}
+
+void task_remove_fd(int fd) {
+    if (fd < 0 || fd >= OPEN_FILE_NUM) {
+        return;
+    }
+    task_current()->file_table[fd] = (file_t*)0;
+    return;
 }
 
 // changes //
@@ -305,13 +767,15 @@ void task_set_unsleep(task_t *task) {
 // main_task's physical address is still within kernel
 
 // 4. copy main_task code from kernel to 0x80000000
-// why copy instead of mapping?
+// why copying instead of mapping? both are fine
 
 // 5. make tasks unable to access kernel code, otherwise it would be dangerous
 // since we're using flat model (set segment regs as 0 and seg size as 0xFFFFFFFF)
 // which means we can't simply utilize segmentation to protect kernel
 // so we use the U/S bit of pte/pde to preovide protection
 // U/S bit = 0 => privilege level 0, 1, 2 can access, U/S bit = 1 => privilege 3 can access
+
+// 6. have to setup privilege level 0 stack and privilege level 3 stack for created tasks
 
 // * page entry without write enable can still be written when privilege level is high (e.g. zero level)
 // so it is used to control privilege level 3
--- source/kernel/include/core/task.h
+++ source/kernel/include/core/task.h
@@ -3,6 +3,7 @@
 
 #include "cpu/cpu.h"
 #include "tools/list.h"
+#include "fs/file.h"
 
 #define TASK_NAME_SIZE 32
 #define TASK_TIME_TICKS_DEFAULT 10
@@ -10,6 +11,7 @@
 #define TASK_FLAG_SYSTEM 1
 #define TASK_FLAG_NORMAL 0
 #define STACK_ZERO_PAGE_COUNT 1
+#define OPEN_FILE_NUM 128
 
 typedef struct _task_t {
     enum {
@@ -18,16 +20,25 @@ typedef struct _task_t {
         TASK_SLEEP,
         TASK_READY,
         TASK_WAITING,
+        TASK_ZOMBIE,
     }state;
+    int status;
+    uint32_t pid;
     char name [TASK_NAME_SIZE];
-    uint32_t *stack;
+    struct _task_t *parent; // should use struct _task because task_t is not seen yet
+    // uint32_t *stack; for simple switch
     tss_t tss;   // used when using hardware to switch tasks
     int tss_sel; // used when using hardware to switch tasks
-    int curr_tick; // curr_tick counts from 10 to 0, then reset as 10 again
+    int curr_tick; // curr_tick counts from 10 to 0, then reset to 10 again
     int sleep_tick;
     list_node_t all_node;
-    list_node_t wait_node; // for wait list
+    // list_node_t wait_node; // for wait list
     list_node_t run_node; // this will move between ready/sleep list
+
+    uint32_t heap_start;
+    uint32_t heap_end;
+
+    file_t *file_table[OPEN_FILE_NUM];
 }task_t;
 
 typedef struct {
@@ -41,20 +52,36 @@ typedef struct {
     int task_data_sel;
 }task_manager_t;
 
-void task_init(task_t *task, const char *name, int flag, uint32_t entry, uint32_t esp);
+typedef struct _task_args_t {
+    uint32_t ret_addr;
+    uint32_t argc;
+    char **argv;
+}task_args_t;
+
+int task_init(task_t *task, const char *name, int flag, uint32_t entry, uint32_t esp);
 void task_switch_from_to(task_t *from, task_t *to);
 void task_manager_init(void);
 void main_task_init(void);
 task_t *task_main_task(void);
 void task_set_ready(task_t *task);
 void task_set_unready(task_t *task);
-void sys_sched_yield();
+void sys_yield();
 void task_dispatch(void);
 task_t *task_next_run(void);
 task_t *task_current(void);
 void task_time_tick(void);
 void task_set_sleep(task_t *task, int ticks);
 void task_set_unsleep(task_t *task);
-void sys_sleep(uint32_t ms);
+void sys_msleep(uint32_t ms);
+uint32_t sys_getpid(void);
+int sys_fork(void);
+void sys_print_msg(const char *fmt, int arg);
+int sys_execve(char *name, char **argv, char **env);
+void sys_exit(int status);
+int sys_wait(int *status);
+
+file_t *task_file(int fd);
+int task_alloc_fd(file_t *file);
+void task_remove_fd(int fd);
 
 #endif
